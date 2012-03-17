4c7961694f38393837227cac277561652aef7c9f
ynonp <ynonperek@gmail.com>
2012-03-17 15:40:43 +0200

    finish html frontend

--- proj/lib/proj.pm
+++ proj/lib/proj.pm
@@ -1,32 +1,74 @@
 package proj;
 use Dancer ':syntax';
 use Dancer::Plugin::Mongo;
+use URI;
 
 our $VERSION = '0.1';
 
-post '/upload' => sub {
-    my $user = param 'user';
-    my $img  = upload 'image';
+get '/' => sub {
+    template 'index';
+};
 
-    my $img_data = $img->content;
-    my $obj = {
-        user => $user,
-        img  => $img_data,
-    };
-    mongo->database->collection->insert($obj);
+get '/upload' => sub {
+    template 'upload';
+};
+
+post '/upload' => sub {
+    my $user = param 'username';
+    my $desc = param 'desc';
+    my $img  = upload 'imagefile';
+
+    my $u = URI->new("data:");
+
+    $u->media_type ( $img->type    );
+    $u->data       ( $img->content );
+
+    insert_image($user, $desc, $u);
+};
+
+post '/images/upload' => sub {
+    my $user = param 'username';
+    my $desc = param 'desc';
+    my $img  = param 'image_data';
+
+    insert_image($user, $desc, $img);
+};
+
+get '/images/:user.json' => sub {
+    my $user = param 'user';
+    debug 'user = ' . $user;
+
+    my @images = get_images_for_user($user);
+
+    to_json { images => [@images] };
 };
 
 get '/images/:user' => sub {
     my $user = param 'user';
-    debug 'user = ' . $user;
-    my $cursor = mongo->database->collection->find({ user => $user });
-    my @objects = $cursor->all;
+    my @images = get_images_for_user($user);
 
-    my $result = {
-        images => [@objects]
+    template 'images', { images => [@images], user => $user };
+};
+
+
+sub get_images_for_user {
+    my $user = shift;
+
+    my $cursor = mongo->rdb->images->find({ user => $user });
+    my @objects = $cursor->all;
+    return @objects;
+}
+
+sub insert_image {
+    my ($user, $desc, $img_data) = @_;
+
+    my $obj = {
+        user => $user,
+        img  => $img_data,
+        desc => $desc,
     };
 
-    return to_json($result);
-};
+    mongo->rdb->images->insert($obj);
+}
 
 true;
